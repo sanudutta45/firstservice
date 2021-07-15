cd841da5da0e3c3917a81499eda61ee6102da23c
Sanu <sanu@stream.space>
2021-07-15 15:34:19 +0530

    new changes

---
+++ public/component---src-pages-404-js-0beb368470e2683ed98e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Page not found 404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0beb368470e2683ed98e.js.map
